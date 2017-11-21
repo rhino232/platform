41eef2433d3db51f125a5b5a72d15dc078f4c822
Andi Birrer <abirrer232@gmail.com>
2017-11-21 22:56:28 +0100

    fixed typo

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "plattform.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
