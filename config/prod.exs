95fc50d4fa2699a782c708e79afd69ecce8be985
Andi Birrer <abirrer232@gmail.com>
2017-11-21 22:59:19 +0100

    another typo

--- config/prod.exs
+++ config/prod.exs
@@ -64,7 +64,7 @@ config :logger, level: :info
 # which should be versioned separately.
 # import_config "prod.secret.exs"
 
-config :platform, Platform.Repo
+config :platform, Platform.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
