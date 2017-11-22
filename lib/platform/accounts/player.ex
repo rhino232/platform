28d450b24928e26b63d35cffba65b55cfb0c8acb
Andi Birrer <abirrer232@gmail.com>
2017-11-22 22:59:49 +0100

    Update player fields and adjust templates

--- lib/platform/accounts/player.ex
+++ lib/platform/accounts/player.ex
@@ -3,8 +3,10 @@ defmodule Platform.Accounts.Player do
   import Ecto.Changeset
   alias Platform.Accounts.Player
 
-
   schema "players" do
+    field :display_name, :string
+    field :password, :string, virtual: true
+    field :password_digest, :string
     field :score, :integer
     field :username, :string
 
@@ -14,7 +16,7 @@ defmodule Platform.Accounts.Player do
   @doc false
   def changeset(%Player{} = player, attrs) do
     player
-    |> cast(attrs, [:username, :score])
-    |> validate_required([:username, :score])
+    |> cast(attrs, [:display_name, :password, :score, :username])
+    |> validate_required([:username])
   end
 end
